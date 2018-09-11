212b1496670c9844769c0d94215b25bea28a292e
Aaron Martin <aaron.martin@shopgate.com>
2018-09-11 15:22:24 -0500

    CCP-765: updated pipeline hook and improved some jsDoc

--- frontend/selectors/product.js
+++ frontend/selectors/product.js
@@ -5,6 +5,7 @@ import {
 } from '@shopgate/pwa-common-commerce/product/selectors/product';
 
 /**
+ * Gets the extra price info from product
  * @param {Object} product Product data.
  * @return {string|null}
  */
@@ -27,16 +28,28 @@ const findExtraPriceInfo = (product) => {
   return null;
 };
 
+/**
+ * Gets extra price info by product id
+ * @return {Object}
+ */
 export const getExtraPriceInfoByProductId = createSelector(
   getProductById,
   product => findExtraPriceInfo(product)
 );
 
+/**
+ * Gets price of product by product id
+ * @return {Object}
+ */
 export const getProductPriceById = createSelector(
   getProductById,
   product => product.productData.price
 );
 
+/**
+ * Gets extra price info of current product
+ * @return {Object}
+ */
 export const getCurrentProductExtraPriceInfo = createSelector(
   getCurrentProduct,
   product => findExtraPriceInfo(product)
