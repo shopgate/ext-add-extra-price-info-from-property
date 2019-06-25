0df6a7bfced6384d9425e4a3482a7a30a5dedb32
Szymon Nowicki <sznowicki@users.noreply.github.com>
2019-06-25 08:56:25 +0200

    PI-9651 - v.2.0.2 style fix for product grid basic price (#5)
    * PI-9651 - v.2.0.2-alpha.1 - style fix for product grid basic price
    
    * PI-9651 - v.2.0.2 - style fix for product grid basic price

--- frontend/portals/category/CategoryItemPrice/components/PriceWrapperGrid/style.js
+++ frontend/portals/category/CategoryItemPrice/components/PriceWrapperGrid/style.js
@@ -1,19 +1,19 @@
-import { css } from 'glamor';
+import {css} from 'glamor';
 
 const priceWrapper = css({
-  lineHeight: 1.75,
+    lineHeight: 1.75,
 }).toString();
 
 const extraPriceInfoWrapper = css({
-  fontSize: '0.70rem',
-  lineHeight: 0.75,
-  marginTop: 1,
-  marginBottom: 3,
+    fontSize: '0.70rem',
+    lineHeight: 0.75,
+    marginTop: 1,
+    marginBottom: 3,
 }).toString();
 
 const basicPrice = css({
-  fontSize: '0.85em',
-  marginTop: -1,
+    fontSize: '0.70em',
+    marginTop: 2.2,
 }).toString();
 
-export default { priceWrapper, extraPriceInfoWrapper, basicPrice };
+export default {priceWrapper, extraPriceInfoWrapper, basicPrice};
